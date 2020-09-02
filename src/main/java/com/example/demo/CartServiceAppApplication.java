46b8c440e98b3f2fd7198f64ff00dbd553b1249e
naveensharma2006 <57703369+naveensharma2006@users.noreply.github.com>
2020-09-02 13:57:50 +0530

    Commit

--- src/main/java/com/example/demo/CartServiceAppApplication.java
+++ src/main/java/com/example/demo/CartServiceAppApplication.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author naveensharma
+ *
+ */
 @SpringBootApplication
 public class CartServiceAppApplication {
 
